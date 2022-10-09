2600af9b59ac592ef4bb01f936237c0b5e750997
cristian-santiago <cristian.santiago.carvalho@gmail.com>
2022-10-09 19:57:13 -0300

    added node_modules

---
+++ app/javascript/application.js
@@ -0,0 +1,13 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "@rails/actiontext"
+//= require jquery3
+//= require jquery-ui
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
+//= require_tree .
+
+
+
